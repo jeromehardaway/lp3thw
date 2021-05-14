f28013a444d24ea5ac820c95cef19053f85ee40c
Jerome Hardaway <jerome@vetswhocode.io>
2021-05-14 16:04:10 -0400

    ex13 complete, learned about imports and how to pass varibales to the file

--- ex13.py
+++ ex13.py
@@ -1,5 +1,8 @@
-age = input("How old are you?")
-height = input("How tall are you?")
-weight = input("How much do you weigh?")
+from sys import argv
 
-print(f" So you are {age} years old, {height} tall, and you weigh {weight}.")
+script, first, second, third = argv
+
+print("The script is called: ", script)
+print("Your first variable is: ", first)
+print("Your second variable is: ", second)
+print("Your third variable is: ", third)
